0762d3ada11d2f8fb4144c806fe0dc44ac4edf71
Simon <simon@bitcartel.com>
2018-01-18 09:33:25 -0800

    Remove assert checking that vpub_old > vpub_new since when counting over a range of blocks, and not from the genesis block, its possible that the blocks only contain unshielding operations.

--- sproutcounter.py
+++ sproutcounter.py
@@ -29,8 +29,7 @@ for i in xrange(block_from, block_to):
             sum_vpub_new += js["vpub_new"]
 
 n = sum_vpub_old - sum_vpub_new
-assert(n >= 0.0)
-print "aggregate value entering sprout circuit    = %.8f" % (sum_vpub_old)
-print "aggregate value leaving sprout circuit     = %.8f" % (sum_vpub_new)
-print "shielded value in sprout circuit = %.8f" % (n)
+print "aggregate value entering sprout circuit     = %.8f" % (sum_vpub_old)
+print "aggregate value leaving sprout circuit      = %.8f" % (sum_vpub_new)
+print "net shielded value change in sprout circuit = %.8f" % (n)
 
