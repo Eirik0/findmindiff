aede7e45dc0eeaf90803515e5eae376db1661ed9
Eirik0 <eirik@z.cash>
2018-05-01 15:38:34 -0600

    Get information for transactions

--- privacymetrics.py
+++ privacymetrics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from decimal import *
-import sys
+from os.path import expanduser
+import sys, os
 
 if len(sys.argv) != 3:
     print "Usage: privacymetrics.py BLOCKFROM BLOCKTO"
@@ -10,26 +10,71 @@ if len(sys.argv) != 3:
 # have a local running instance of zcashd
 api = AuthServiceProxy("http://username:password@127.0.0.1:8232")
 
-count_joinsplits = 0
-count_shielded_tx = 0
 block_from = int(sys.argv[1])
-block_to = int(sys.argv[2]) + 1  # +1 so inclusive
-sum_vpub_old = Decimal(0)
-sum_vpub_new = Decimal(0)
+block_to = int(sys.argv[2])
 
-for i in xrange(block_from, block_to):
-    blockhash = api.getblockhash(i)
-    block = api.getblock(blockhash)
+filePath = expanduser("~") + "/zcash_stats/"
+fileName = "stats{}-{}.csv".format(block_from, block_to)
+if not os.path.exists(filePath):
+    os.makedirs(filePath)
+statsFile = open(filePath + fileName, "w+")
+
+statsFile.write("blockHeight,blockTime,txId,isCoinBase,numVIn,totalVIn,numVOut,totalVOut,numJS,totalJSIn,totalJSOut,isSpendingCoinBase,type\n")
+
+for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
+    block = api.getblock("%d" % blockHeight)
+    blockTime = block["time"]
     for txid in block["tx"]:
-        tx = api.getrawtransaction(txid,1)
-        if len(tx["vjoinsplit"]) is 0:
+        try:
+            tx = api.getrawtransaction(txid, 1)
+        except JSONRPCException as e:
+            print "In block {}, txid {}: {}\n".format(blockHeight, txid, e)
             continue
-        for js in tx["vjoinsplit"]:
-            sum_vpub_old += js["vpub_old"]
-            sum_vpub_new += js["vpub_new"]
 
-n = sum_vpub_old - sum_vpub_new
-print "aggregate value entering sprout circuit     = %.8f" % (sum_vpub_old)
-print "aggregate value leaving sprout circuit      = %.8f" % (sum_vpub_new)
-print "net shielded value change in sprout circuit = %.8f" % (n)
+        isCoinBase = len(tx["vin"]) == 1 and "coinbase" in tx["vin"][0]
+
+        totalVIn = 0
+        isSpendingCoinBase = False
+        if not isCoinBase:
+            for vin in tx["vin"]:
+                prevHash = vin["txid"]
+                prevN = vin["vout"]
+                try:
+                    prevTx = api.getrawtransaction(prevHash, 1)
+                except JSONRPCException as e:
+                    print "When loading prevout {}, txid {}: {}\n".format(prevHash, txid, e)
+                    continue
+                totalVIn += prevTx["vout"][prevN]["value"]
+                isSpendingCoinBase |= len(prevTx["vin"]) == 1 and "coinbase" in prevTx["vin"][0]
+
+        totalVOut = 0
+        for vout in tx["vout"]:
+            totalVOut += vout["value"]
+
+        totalJSIn = 0
+        totalJSOut = 0
+        for js in tx["vjoinsplit"]:
+            totalJSIn += js["vpub_new"]
+            totalJSOut += js["vpub_old"]
+
+        txType = "tz"
+        if len(tx["vjoinsplit"]) == 0:
+            txType = "tt"
+        elif len(tx["vin"]) == 0 and len(tx["vout"]) == 0:
+            txType = "tz"
+        elif totalJSIn > 0:
+            txType = "zt"
+
+        formatVIn = "{0:f}".format(totalVIn)
+        formatVOut = "{0:f}".format(totalVOut)
+        formatJSIn = "{0:f}".format(totalJSIn)
+        formatJSOut = "{0:f}".format(totalJSOut)
+
+        statsFile.write("{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(blockHeight,blockTime,txid,isCoinBase,
+                len(tx["vin"]), formatVIn, len(tx["vout"]), formatVOut, 
+                len(tx["vjoinsplit"]), formatJSIn, formatJSOut,
+                isSpendingCoinBase, txType))
+
+statsFile.flush()
+statsFile.close()
 
