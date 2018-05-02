3ad18347ad5f03b59ecaa9824f22c1f7391386e8
Eirik0 <eirik@z.cash>
2018-05-02 16:08:31 -0600

    Add error logging and handling

--- privacymetrics.py
+++ privacymetrics.py
@@ -2,6 +2,11 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from os.path import expanduser
 import os, sys, time
+# The following import is from authproxy,py
+try:
+    import http.client as httplib
+except ImportError:
+    import httplib
 
 class CachedTx:
     def __init__(self, tx):
@@ -22,37 +27,60 @@ block_from = min(max(0, block_from), chainHeight)
 block_to = min(max(block_to, block_from), chainHeight)
 
 filePath = expanduser("~") + "/zcash_stats/"
-fileName = "stats{}-{}.csv".format(block_from, block_to)
 if not os.path.exists(filePath):
     os.makedirs(filePath)
-statsFile = open(filePath + fileName, "w+")
 
-statsFile.write("blockHeight,blockTime,txId,isCoinBase,numVIn,totalVIn,numVOut,totalVOut,numJS,totalJSIn,totalJSOut,isSpendingCoinBase\n")
+statsFileName = "stats{}-{}.csv".format(block_from, block_to)
+statsFile = open(filePath + statsFileName, "w+")
+
+errorFileName = "stats{}-{}_errors.txt".format(block_from, block_to)
+errorFile = open(filePath + errorFileName, "w+")
 
 print "\nloading blocks {}-{}...\n".format(block_from, block_to)
 
 totalBlocks = block_to - block_from + 1
-onePercent = max(int(totalBlocks / 100), 1)
-tenPercent = max(int(totalBlocks / 10), 1)
-numBlocks = 1
+onePercent = max(int(round(totalBlocks / 100)), 1)
+tenPercent = max(int(round(totalBlocks / 10)), 1)
+numBlocks = 0
 numTxs = 0
+numErrors = 0
 
 cachedTxs = dict()
 
 start_time = time.time()
 last_update_time = start_time
+lastNumErrors = 0
+lastNumTxs = 0
+
+statsFile.write("blockHeight,blockTime,txId,isCoinBase,numVIn,totalVIn,numVOut,totalVOut,numJS,totalJSIn,totalJSOut,isSpendingCoinBase\n")
 
 for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
-    if (numBlocks % onePercent) == 0:
+    if numBlocks > 0 and numBlocks % onePercent == 0:
         sys.stdout.write(".")
         sys.stdout.flush()
-    if (numBlocks % tenPercent) == 0:
+    if numBlocks > 0 and numBlocks % tenPercent == 0:
         current_time = time.time()
         elapsed = int(current_time - last_update_time)
-        print "+{:02d}:{:02d}:{:02d}".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60)
+        print " +{:02d}:{:02d}:{:02d} ({} txs, {} errors)".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60, numTxs - lastNumTxs, numErrors - lastNumErrors)
         last_update_time = current_time
+        lastNumTxs = numTxs
+        lastNumErrors = numErrors
 
-    block = api.getblock("{}".format(blockHeight), 2)
+    block = None
+    for attempt in xrange(1, 3): # 2 tries
+        try:
+            block = api.getblock("{}".format(blockHeight), 2)
+            break
+        except httplib.BadStatusLine as e:
+            numErrors += 1
+            errorFile.write("Error when loading block {}: {}\n".format(blockHeight, e.message))
+            if attempt == 1:
+                time.sleep(1)
+
+    if block is None:
+        errorFile.write("Skipping block {}\n".format(blockHeight))
+        continue   
+
     blockTime = block["time"]
     for tx in block["tx"]:
         txid = tx["txid"]
@@ -67,7 +95,7 @@ for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
             for vin in tx["vin"]:
                 prevHash = vin["txid"]
                 prevN = vin["vout"]
-                if prevHash in cachedTxs:
+                if cachedTxs.has_key(prevHash):
                     prevTx = cachedTxs[prevHash]
                     totalVIn += prevTx.values[prevN]
                     isSpendingCoinBase |= prevTx.isCoinBase
@@ -75,7 +103,8 @@ for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
                     try:
                         prevTx = api.getrawtransaction(prevHash, 1)
                     except JSONRPCException as e:
-                        print "When loading prevout {}, txid {}: {}\n".format(prevHash, txid, e)
+                        numErrors += 1
+                        errorFile.write("Error when loading prevout {} for transaction {}: {}\n".format(prevHash, txid, e))
                         continue
                     totalVIn += prevTx["vout"][prevN]["value"]
                     isSpendingCoinBase |= len(prevTx["vin"]) == 1 and "coinbase" in prevTx["vin"][0]
@@ -100,7 +129,7 @@ for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
         formatSpendingCoinBase = "T" if isSpendingCoinBase else "F"
 
         statsFile.write("{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
-                blockHeight,blockTime, txid, formatCoinBase,
+                blockHeight, blockTime, txid, formatCoinBase,
                 len(tx["vin"]), formatVIn, len(tx["vout"]), formatVOut, 
                 len(tx["vjoinsplit"]), formatJSIn, formatJSOut,
                 formatSpendingCoinBase))
@@ -114,4 +143,12 @@ statsFile.close()
 
 elapsed = int(time.time() - start_time)
 elapsed_time_str = "{:02d}:{:02d}:{:02d}".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60)
-print "\n{} transactions written to {} in {}".format(numTxs, filePath + fileName, elapsed_time_str)
+print "\n{} transactions written to {} in {}".format(numTxs, filePath + statsFileName, elapsed_time_str)
+if numErrors > 0:
+    print "{} errors written to {}".format(numErrors, filePath + errorFileName)
+else:
+    errorFile.write("No errors\n")
+    print "with no errors"
+
+errorFile.flush()
+errorFile.close()
