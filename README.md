6b202f0721bc3342cda600b256014a8b2666fcad
Eirik0 <eirik@z.cash>
2018-10-10 13:52:05 -0600

    Modify script to output min diff blocks

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
-## Privacy Metrics
+## Find Min Diff
 
-This program outputs a csv file with information about each transaction in the
-Zcash blockchain.
-
-The columns are as follows:
-
-* blockHeight - The height of the block containing the transaction
-* blockTime - The time the block was mined
-* txId - The ID of the transaction
-* isCoinBase - 'T' if the transaction is a coinbase transaction
-* numVIn - The number of vin for the transaction
-* totalVIn - The sum of the values referenced by the vins
-* numVOut - The number of vout for the transaction
-* totalVOut - The sum of the values of the vouts
-* numJS - The number of joinsplits for the transaction
-* totalJSIn - The sum of the shielded input amount
-* totalJSOut - The sum of the shielded output amounts
-* isSpendingCoinBase - 'T' if the vins reference a coinbase transaction
+This program will examine the blockchain and output a list of all blocks which
+have minimal difficulty over a given range.
 
 # Requirements
 
@@ -26,86 +11,34 @@ pip install python-bitcoinrpc
 
 Start a Zcash daemon and the execute the following command
 
-python privacymetrics.py (BLOCKFROM) (BLOCKTO)
+python findmindiff.py (BLOCKFROM) (BLOCKTO)
 
-# Examples
+# Example
 
-python privacymetrics.py 0 1000
+python findmindiff.py 0 1000
 
-    loading blocks 0-1000...
+    loading blocks 290000-303587...
 
-    .......... +00:00:00 (100 txs, 0 errors)
-    .......... +00:00:00 (100 txs, 0 errors)
-    .......... +00:00:00 (100 txs, 0 errors)
-    .......... +00:00:00 (102 txs, 0 errors)
-    .......... +00:00:00 (145 txs, 0 errors)
-    .......... +00:00:01 (355 txs, 0 errors)
-    .......... +00:00:02 (421 txs, 0 errors)
-    .......... +00:00:01 (304 txs, 0 errors)
-    .......... +00:00:02 (491 txs, 0 errors)
-    .......... +00:00:02 (638 txs, 0 errors)
+    .......... +00:00:07
+    .......... +00:00:08
+    .......... +00:00:07
+    .......... +00:00:08
+    .......... +00:00:07
+    .......... +00:00:09
+    .......... +00:00:07
+    .......... +00:00:07
+    .......... +00:00:07
+    .......... +00:00:07
 
-    2758 transactions written to /home/eirik/zcash_stats/stats0-1000.csv in 00:00:13
-    0 errors written to /home/eirik/zcash_stats/stats0-1000_errors.txt
-
-python privacymetrics.py 316900
-
-    loading blocks 316900-316928...
-
-    .. +00:00:37 (55 txs, 0 errors)
-    .. +00:00:20 (13 txs, 0 errors)
-    .. +00:00:04 (22 txs, 0 errors)
-    .. +00:03:24 (31 txs, 0 errors)
-    .. +00:00:11 (48 txs, 0 errors)
-    .. +00:00:01 (26 txs, 0 errors)
-    .. +00:01:13 (22 txs, 0 errors)
-    .. +00:00:02 (23 txs, 0 errors)
-    .. +00:00:01 (13 txs, 0 errors)
-    .. +00:00:10 (71 txs, 0 errors)
-    .. +00:00:01 (9 txs, 0 errors)
-    .. +00:01:19 (15 txs, 0 errors)
-    .. +00:00:02 (33 txs, 0 errors)
-    .. +00:00:11 (30 txs, 0 errors)
-
-    418 transactions written to /home/eirik/zcash_stats/stats316900-316928.csv in 00:07:43
-    0 errors written to /home/eirik/zcash_stats/stats316900-316928_errors.txt
-
-
-python privacymetrics.py 
-
-    loading blocks 0-316566...
-
-    .......... +00:17:06 (183378 txs, 0 errors)
-    .......... +00:16:46 (126591 txs, 0 errors)
-    .......... +00:16:25 (155691 txs, 0 errors)
-    .......... +00:23:04 (211800 txs, 0 errors)
-    .......... +00:21:43 (262748 txs, 0 errors)
-    .......... +00:24:44 (301184 txs, 0 errors)
-    .......... +00:26:21 (344080 txs, 0 errors)
-    .......... +00:51:59 (563301 txs, 2 errors)
-    .......... +00:58:12 (472586 txs, 1 errors)
-    .......... +01:34:22 (341515 txs, 2 errors)
-
-    2962940 transactions written to /home/user/zcash_stats/stats0-316566.csv in 05:50:46
-    5 errors written to /home/user/zcash_stats/stats0-316566_errors.txt
+    Finished in 00:01:18
+    Min diff = 1
+    299188: diff=1, time=3351
+    299189: diff=1, time=1104
+    299202: diff=1, time=14581
+    ... And so on ...
 
 # Notes
 
 There are two optional parameters BLOCKFROM and BLOCKTO. The default for
 BLOCKFROM is 0 and the default for BLOCKTO is the current height of the
 blockchain
-
-The program will print to the console based on it's progress though the 
-blockchain. It will print the time elapsed, the number of transactions
-written, and the number of errors encountered for each ~1/10th of the
-requested range of blocks. When the program has finished processing it 
-will print the number of transactions processed, the location of the
-output file, the total time elapsed, and a message regarding whether or
-not there were any errors.
-
-The program is designed to be able to recover from some errors. If there is
-an error when making the a call for a raw transaction, the 'totalVIn' for that
-transaction will not be correct. If there is an error when calling for a block. 
-The program will open a new connection and try to make the call again. If 
-there is only one error for a given block, assuming the program did not crash, 
-then the program will have recovered and successfully loaded the block.
--- findmindiff.py
+++ findmindiff.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python2
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from os.path import expanduser
-import os, sys, time
-# The following import is from authproxy,py
-try:
-    import http.client as httplib
-except ImportError:
-    import httplib
-
-class CachedTx:
-    def __init__(self, tx):
-        self.isCoinBase = len(tx["vin"]) == 1 and "coinbase" in tx["vin"][0]
-        self.values = []
+from bitcoinrpc.authproxy import AuthServiceProxy
+import sys
+import time
 
 if len(sys.argv) > 3:
-    print "Usage: privacymetrics.py (BLOCKFROM) (BLOCKTO)"
+    print "Usage: findmindiff.py (BLOCKFROM) (BLOCKTO)"
     exit(1)
+
+
+class BlockInfo:
+    def __init__(self, block_num, diff, time):
+        self.block_num = block_num
+        self.diff = diff
+        self.time = time
+
+    def __str__(self):
+        return "{}: diff={}, time={}".format(self.block_num, self.diff, self.time)
+
 
 # have a local running instance of zcashd
 api = AuthServiceProxy("http://username:password@127.0.0.1:8232")
@@ -26,131 +27,66 @@ block_to = int(sys.argv[2]) if len(sys.argv) > 2 else chainHeight
 block_from = min(max(0, block_from), chainHeight)
 block_to = min(max(block_to, block_from), chainHeight)
 
-filePath = expanduser("~") + "/zcash_stats/"
-if not os.path.exists(filePath):
-    os.makedirs(filePath)
-
-statsFileName = "stats{}-{}.csv".format(block_from, block_to)
-statsFile = open(filePath + statsFileName, "w+")
-
-errorFileName = "stats{}-{}_errors.txt".format(block_from, block_to)
-errorFile = open(filePath + errorFileName, "w+")
-
 print "\nloading blocks {}-{}...\n".format(block_from, block_to)
 
 totalBlocks = block_to - block_from + 1
 onePercent = max(int(round(totalBlocks / 100)), 1)
 tenPercent = max(int(round(totalBlocks / 10)), 1)
 numBlocks = 0
-numTxs = 0
-numErrors = 0
-
-cachedTxs = dict()
 
 start_time = time.time()
 last_update_time = start_time
-lastNumErrors = 0
-lastNumTxs = 0
 
-statsFile.write("blockHeight,blockTime,txId,isCoinBase,numVIn,totalVIn,numVOut,totalVOut,numJS,totalJSIn,totalJSOut,isSpendingCoinBase\n")
+min_diff = None
+min_diff_blocks = []
 
-for blockHeight in xrange(block_from, block_to + 1): # +1 so inclusive
+last_block_time = None
+
+for block_height in xrange(block_from, block_to + 1):  # +1 so inclusive
     if numBlocks > 0 and numBlocks % onePercent == 0:
         sys.stdout.write(".")
         sys.stdout.flush()
     if numBlocks > 0 and numBlocks % tenPercent == 0:
         current_time = time.time()
         elapsed = int(current_time - last_update_time)
-        print " +{:02d}:{:02d}:{:02d} ({} txs, {} errors)".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60, numTxs - lastNumTxs, numErrors - lastNumErrors)
+        print " +{:02d}:{:02d}:{:02d}".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60)
         last_update_time = current_time
-        lastNumTxs = numTxs
-        lastNumErrors = numErrors
 
     block = None
-    for attempt in xrange(1, 3): # 2 tries
+    for attempt in xrange(1, 3):  # 2 tries
         try:
-            block = api.getblock("{}".format(blockHeight), 2)
+            block = api.getblock("{}".format(block_height), 2)
             break
         except Exception as e:
-            numErrors += 1
-            errorFile.write("Error when loading block {} - {}: {}\n".format(blockHeight, type(e), e.message))
             # Create a new proxy as we may not be able to send more requests without a new connection
             api = AuthServiceProxy("http://username:password@127.0.0.1:8232")
 
     if block is None:
-        errorFile.write("Skipping block {}\n".format(blockHeight))
-        continue   
+        print "Skipping block {}\n".format(block_height)
+        continue
 
-    blockTime = block["time"]
-    for tx in block["tx"]:
-        txid = tx["txid"]
-        
-        cachedTx = CachedTx(tx)
-        cachedTxs[txid] = cachedTx
+    diff = block["difficulty"]
+    block_time = block["time"]
 
-        totalVIn = 0
-        isSpendingCoinBase = False
+    block_time_elapsed = None
+    if last_block_time is not None:
+        block_time_elapsed = block_time - last_block_time
 
-        if not cachedTx.isCoinBase:
-            for vin in tx["vin"]:
-                prevHash = vin["txid"]
-                prevN = vin["vout"]
-                cachedPrevTx = None
-                if cachedTxs.has_key(prevHash):
-                    cachedPrevTx = cachedTxs[prevHash]
-                else:
-                    try:
-                        prevTx = api.getrawtransaction(prevHash, 1)
-                    except JSONRPCException as e:
-                        numErrors += 1
-                        errorFile.write("Error when loading prevout {} for transaction {}: {}\n".format(prevHash, txid, e))
-                        continue
-                    cachedPrevTx = CachedTx(prevTx)
-                    cachedTxs[prevHash] = cachedPrevTx
-                    for vout in prevTx["vout"]:
-                        cachedPrevTx.values.append(vout["value"])
-                totalVIn += cachedPrevTx.values[prevN]
-                isSpendingCoinBase |= cachedPrevTx.isCoinBase
-
-        totalVOut = 0
-        for vout in tx["vout"]:
-            value = vout["value"]
-            cachedTx.values.append(value)
-            totalVOut += value
-
-        totalJSIn = 0
-        totalJSOut = 0
-        for js in tx["vjoinsplit"]:
-            totalJSIn += js["vpub_new"]
-            totalJSOut += js["vpub_old"]
-
-        formatVIn = "{0:.8f}".format(totalVIn)
-        formatVOut = "{0:.8f}".format(totalVOut)
-        formatJSIn = "{0:.8f}".format(totalJSIn)
-        formatJSOut = "{0:.8f}".format(totalJSOut)
-        formatCoinBase = "T" if cachedTx.isCoinBase else "F"
-        formatSpendingCoinBase = "T" if isSpendingCoinBase else "F"
-
-        statsFile.write("{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
-                blockHeight, blockTime, txid, formatCoinBase,
-                len(tx["vin"]), formatVIn, len(tx["vout"]), formatVOut, 
-                len(tx["vjoinsplit"]), formatJSIn, formatJSOut,
-                formatSpendingCoinBase))
-
-        numTxs += 1
+    if min_diff is None or diff < min_diff:
+        min_diff = diff
+        min_diff_blocks = [BlockInfo(block_height, diff, block_time_elapsed)]
+    elif diff == min_diff:
+        min_diff_blocks.append(BlockInfo(block_height, diff, block_time_elapsed))
     
-    numBlocks += 1
+    last_block_time = block_time
 
-statsFile.flush()
-statsFile.close()
+    numBlocks += 1
 
 elapsed = int(time.time() - start_time)
 elapsed_time_str = "{:02d}:{:02d}:{:02d}".format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60)
-print "\n{} transactions written to {} in {}".format(numTxs, filePath + statsFileName, elapsed_time_str)
 
-if numErrors == 0:
-    errorFile.write("No errors\n")
-print "{} errors written to {}".format(numErrors, filePath + errorFileName)
+print "\nFinished in {}".format(elapsed_time_str)
 
-errorFile.flush()
-errorFile.close()
+print "Min diff = {}".format(min_diff)
+for block_info in min_diff_blocks:
+    print block_info
